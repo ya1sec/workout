85fb91aefc8f6339309770ed6b9b8f7de8de19cd
unknown <theankitbagchi@gmail.com>
2020-08-20 14:59:57 -0400

    Added routes and styles

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+// DEPENDENCIES
+
+var path = require("path");
+
+module.exports = function (app) {
+  // Called when "Countinue Workout" or "new Workout" is clicked in index.html
+  app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
